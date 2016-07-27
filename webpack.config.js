7621994f39a12ddf61d1344c8e830db462d4e56a
xusheng-antiy <xusheng@antiy.cn>
2016-07-27 19:49:03 +0800

    '增加了loader和plugin'

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,46 @@
+const webpack = require('webpack')
+
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: './bin',
-    filename: 'app.bundle.js'
-  }
+    filename: 'app.bundle.js',
+    path: './bin'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: 'node_modules/',
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  ]
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
